dc8b7bc8738b2d6619dae22b5b137378220d1e89
Hamza-ALyemeni <hamzaalchamak442979@gmail.com>
2024-09-28 23:42:59 +0300

    validation forms on keystroks or blur

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,19 +6,40 @@ export default function Login() {
     password:''
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email:false,
+    password:false
+  })
+
+  const emailIsInvalid = didEdit.email && !inputChange.email.includes('@');
+
   function handleSubmit(e) {
     e.preventDefault();
 
     console.log(inputChange);
   }
 
-  function handleInputChange(identifers,event) {
+  function handleInputChange(identifers,value) {
     setInputChange((prevInput) => ({
       ...prevInput,
-      [identifers]: event.target.value
+      [identifers]: value
     })
-    ) 
+    )
+    setDidEdit((prevInput) => ({
+      ...prevInput,
+      [identifers]: false
+    })
+    )
   }
+
+  function handleBlur(identifers) {
+    setDidEdit((prevInput) => ({
+      ...prevInput,
+      [identifers]: true
+    })
+    )
+  }
+  
 
   return (
     <form onSubmit={handleSubmit}>
@@ -27,15 +48,23 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" 
-          onChange={(event) => handleInputChange('email',event)} 
+          <input 
+          id="email" 
+          type="email"
+          name="email" 
+          onBlur={() => handleBlur('email')}
+          onChange={(event) => handleInputChange('email',event.target.value)} 
           value={inputChange.email}/>
+          <div className="control-error">{emailIsInvalid && <p>please provide a vlide email address</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password"
-          onChange={(event) => handleInputChange('password',event)}
+          <input
+          id="password"
+          type="password" 
+          name="password"
+          onChange={(event) => handleInputChange('password',event.target.value)}
           value={inputChange.password} />
         </div>
       </div>
